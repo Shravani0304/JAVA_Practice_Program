e7ca31ccf0970e98358ac082ddd1532eb35d6fee
Shravani0304 <shravanithorat030@gmail.com>
2025-11-10 19:29:36 +0530

    program to print numbers from 1 to 10 linearly using recursion

--- src/RecursionDemo.java
+++ src/RecursionDemo.java
@@ -6,15 +6,23 @@ public class RecursionDemo {
 //        recursion(++cnt);
 //    }
 
-    public static  void myName(int cnt){
-        if (cnt==5)
+//    public static  void myName(int cnt){
+//        if (cnt==5)
+//            return;
+//        System.out.println("shravani");
+//        myName(++cnt);
+//    }
+    public static  void linearPrint(int num,int n){
+        if(n==num)
             return;
-        System.out.println("shravani");
-        myName(++cnt);
+        System.out.println(n);
+        linearPrint(num,++n);
     }
+
 
     public static void main(String[] args) {
         //recursion(  1);
-        myName(1);
+        //myName(1);
+        linearPrint(11,1);
     }
 }
