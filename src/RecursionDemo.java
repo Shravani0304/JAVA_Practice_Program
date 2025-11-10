40e30a077c023501e594c3f4e787d29fa5825e9d
Shravani0304 <shravanithorat030@gmail.com>
2025-11-10 19:23:27 +0530

    writing another example to print my name 5 times using recursion

--- src/RecursionDemo.java
+++ src/RecursionDemo.java
@@ -1,11 +1,20 @@
 public class RecursionDemo {
 
-    public static void recursion(int cnt){
-        if(cnt==5)return;
-        System.out.println(cnt);
-        recursion(++cnt);
+//    public static void recursion(int cnt){
+//        if(cnt==5)return;
+//        System.out.println(cnt);
+//        recursion(++cnt);
+//    }
+
+    public static  void myName(int cnt){
+        if (cnt==5)
+            return;
+        System.out.println("shravani");
+        myName(++cnt);
     }
+
     public static void main(String[] args) {
-        recursion(  1);
+        //recursion(  1);
+        myName(1);
     }
 }
