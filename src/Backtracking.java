d527606fab70376e505d28521c53427495aca9d2
Shravani0304 <shravanithorat030@gmail.com>
2025-11-10 21:49:56 +0530

    program to print numbers from 1 to 10 linearly using Backtracking

--- src/Backtracking.java
+++ src/Backtracking.java
@@ -1,11 +1,19 @@
 public class Backtracking {
-    public static  void printReverse(int n){
-        if(n==10)return;
-        printReverse(++n);
+//    public static  void printReverse(int n){
+//        if(n==10)return;
+//        printReverse(++n);
+//        System.out.println(n);
+//    }
+
+    public static  void printLinear(int n){
+        if(n==1)return;
+        printLinear(--n);
         System.out.println(n);
+
     }
 
     public static void main(String[] args) {
-        printReverse(0);
+      //  printReverse(0);
+        printLinear(11);
     }
 }
