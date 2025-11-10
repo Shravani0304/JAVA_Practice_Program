fcf9195ffa78ee2c465b648fe2883900d44fecdf
Shravani0304 <shravanithorat030@gmail.com>
2025-11-10 18:41:14 +0530

    Recursion

---
+++ src/RecursionDemo.java
@@ -0,0 +1,11 @@
+public class RecursionDemo {
+
+    public static void recursion(int cnt){
+        if(cnt==5)return;
+        System.out.println(cnt);
+        recursion(++cnt);
+    }
+    public static void main(String[] args) {
+        recursion(  1);
+    }
+}
