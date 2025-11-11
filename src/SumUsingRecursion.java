806e480a0c10a806ddeb735919626ee043856e3f
Shravani0304 <shravanithorat030@gmail.com>
2025-11-11 07:59:41 +0530

    program to print sum of first n numbers using Functional recursion

--- src/SumUsingRecursion.java
+++ src/SumUsingRecursion.java
@@ -1,17 +1,25 @@
 public class SumUsingRecursion{
 
-    public static void sumOfInteger(int i,int sum){
-        if(i<1){
-            System.out.println(sum);
-            return ;
-        }
+//    public static void sumOfInteger(int i,int sum){
+//        if(i<1){
+//            System.out.println(sum);
+//            return ;
+//        }
+//
+//        sumOfInteger(i-1,sum+i);
+//
+//    }
 
-        sumOfInteger(i-1,sum+i);
-
+    public  static  int printSum(int n){
+        if(n==0)
+            return 0;
+        return n+printSum(n-1);
     }
 
     public static void main(String[] args) {
-        sumOfInteger(10,0);
+ //       sumOfInteger(10,0);
+        int n=10;
+        System.out.println( printSum(n));
 
     }
 }
