ea3b3736ee2b2653480dca5da2e68ffa0b1407ab
Shravani0304 <shravanithorat030@gmail.com>
2025-11-11 10:03:42 +0530

    reverse array using single pointer only

--- src/ReverseArray.java
+++ src/ReverseArray.java
@@ -1,19 +1,32 @@
 import java.util.Arrays;
 
 public class ReverseArray {
-    public static  void reverseArray(int [] arr,int start,int end){
-        if(start>=end)return;
-        int temp=arr[start];
-        arr[start]=arr[end];
-        arr[end]=temp;
-        reverseArray(arr,++start,--end);
+//    public static  void reverseArray(int [] arr,int start,int end){
+//        if(start>=end)return;
+//        int temp=arr[start];
+//        arr[start]=arr[end];
+//        arr[end]=temp;
+//        reverseArray(arr,++start,--end);
+//    }
+
+    public static  void reverseArray2(int [] arr,int i,int n){
+        if(i==n/2)return;
+        int temp=arr[i];
+        arr[i]=arr[n-i-1];
+        arr[n-i-1]=temp;
+
+        reverseArray2(arr,i+1,n);
     }
+
 
     public static void main(String[] args) {
         int [] arr={1,2,3,4};
-        System.out.println("before:"+Arrays.toString(arr));
-        reverseArray(arr,0,arr.length-1);
-        System.out.println("after: "+Arrays.toString(arr));
+        int n=arr.length;
+
+        System.out.println("before reverse:"+Arrays.toString(arr));
+        reverseArray2(arr,0,n);
+       // using two pointer approach: reverseArray(arr,0,arr.length-1);
+        System.out.println("after reversing: "+Arrays.toString(arr));
 
     }
 }
